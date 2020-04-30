692b75d18a472a47cbcf0c83b51f034769f3abf3
yliess86 <hatiyliess86@gmail.com>
2020-04-30 14:40:34 +0200

    Reset Repo for new Version

--- facecrop/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""facetorch/__init__.py"""
-from facecrop.annotator import FaceAnnotator
-from facecrop.detector import FaceDetector
-from facecrop.landmark import FaceLandmarker
-from facecrop.video import Video
-from facecrop.visualizer import FaceVisualizer
